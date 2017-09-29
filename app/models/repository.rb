4eb89d85d0d754715d8cd6d9abb1ef7a7c7659eb
simplay <silent.simplay@gmail.com>
2017-09-29 19:04:04 +0200

    Fetch repository's description via gitlab api

--- app/models/repository.rb
+++ app/models/repository.rb
@@ -1,6 +1,4 @@
 class Repository < ApplicationRecord
-  BASE_URL = ''.freeze
-
   before_save :load_data
 
   def to_json
@@ -12,11 +10,31 @@ class Repository < ApplicationRecord
     }
   end
 
+  class GitlabAPI
+    BASE_URL = 'http://gitlab.swissdrg.local/api/v4/'
+
+    attr_reader :url
+    def initialize(url:)
+      @url = url
+    end
+
+    def description
+      request = "#{BASE_URL}projects/#{url}?statistics=true&private_token=#{ENV['PRIVATE_TOKEN']}"
+      response = HTTParty.get(request, timeout: 5)
+      response.body['description']
+    end
+
+    def self.repositories
+      request = "#{BASE_URL}projects?private_token=#{ENV['PRIVATE_TOKEN']}"
+      response = HTTParty.get(request, timeout: 5)
+      response.body
+    end
+  end
+
   private
 
   def load_data
-    self.name = url.split('/').last
-    self.description = ""
+    self.description = GitlabAPI.new(url: url).description
   end
 
 end
