130bde589e988748d5b183cb0504ec87cfb05ae4
simplay <silent.simplay@gmail.com>
2017-09-29 17:44:00 +0200

    Define Repository#GET and POST

--- app/api/v1/repositories.rb
+++ app/api/v1/repositories.rb
@@ -4,28 +4,52 @@ module V1
     version 'v1', using: :path
     format :json
 
-    params do
-      requires :id, type: Integer, desc: ''
-    end
     get 'repositories/' do
-      if false
-      end
+      Repository.all
     end
 
     class Repository
-      attr_reader :id, :name, :description
-      def initialize(id:, name:, description: '')
+      attr_reader :id, :name, :description, :url
+
+      def initialize(id:,
+                     name:,
+                     description: '',
+                     url: '')
         @id = id
         @name = name
         @description = description
+        @url = url
+      end
+
+      def self.all
+        [
+          new(
+            id: -1337,
+            name: "4chan",
+            description: "where the fun begins..",
+            url: "www.4chan.org"
+          )
+        ]
       end
 
       def to_json
         {
           id: id,
           name: name,
-          description: description
+          description: description,
+          url: url
         }
+      end
+
+      def valid?
+        true
+      end
+
+      def save
+      end
+
+      def errors
+        []
       end
     end
 
@@ -33,12 +57,39 @@ module V1
       requires :id, type: Integer, desc: ''
     end
     get 'repositories/:id' do
-      repo = Repository.new(id: 1, name: "manual_printer")
-      if repo
-        repo.to_json
-      else
-        error!("No repository with id #{params[:id]} found.", 404)
+      begin
+        repo = Repository.new(id: 1, name: "manual_printer")
+        if repo.valid?
+          repo.to_json
+        else
+          error!("No repository with id #{params[:id]} found.", 404)
+        end
+      rescue => e
+        error! e.message
       end
     end
+
+    params do
+      requires :url, type: String, desc: 'Url of target repository'
+    end
+    post '/repository' do
+      begin
+        repo = Repository.new(
+          id: -1,
+          name: "foo",
+          description: "fooabr",
+          url: params[:url]
+        )
+
+        if repo.valid?
+          repo.save
+        else
+          error! repo.errors
+        end
+      rescue => e
+        error! e.message
+      end
+    end
+
   end
 end
