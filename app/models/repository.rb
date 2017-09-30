614abd22b8561467d792563c82f017440c1bec33
simplay <silent.simplay@gmail.com>
2017-09-30 16:50:10 +0200

    Export GitlabAPI class to own file

--- app/models/repository.rb
+++ app/models/repository.rb
@@ -18,33 +18,6 @@ class Repository < ApplicationRecord
     }
   end
 
-  class GitlabAPI
-    BASE_URL = 'http://gitlab.swissdrg.local/api/v4/'
-
-    attr_reader :url
-    def initialize(url:)
-      @url = url.gsub('/', '%2F')
-    end
-
-    def repository_details
-      request = "#{BASE_URL}projects/#{url}?statistics=true&private_token=#{ENV.fetch('PRIVATE_TOKEN')}"
-      response = HTTParty.get(request, timeout: 5)
-      (JSON.parse response.body)
-    end
-
-    def self.repositories
-      request = "#{BASE_URL}projects?private_token=#{ENV.fetch('PRIVATE_TOKEN')}"
-      response = HTTParty.get(request, timeout: 5)
-      (JSON.parse response.body)
-    end
-
-    def self.available
-      repos_db = Repository.all.map(&:url)
-      repos_gitlab = repositories.map{ |repo| repo['path_with_namespace']}
-      repos_gitlab - repos_db
-    end
-  end
-
   private
 
   def status_ok?
